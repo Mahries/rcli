e3a1fb1df3ff9ac53bd3c855c22f737980f02e6d
Mahires <loritas.personal@gmail.com>
2025-04-01 00:22:52 +0800

    init & fix deprecated config #1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+/*
+ * @Author: Mahires loritas.personal@gmail.com
+ * @Date: 2025-03-31 19:44:45
+ * @LastEditors: Mahires loritas.personal@gmail.com
+ * @LastEditTime: 2025-04-01 00:03:56
+ * @FilePath: \rcli\src\main.rs
+ * @Description:
+ * Copyright (c) 2025 by Mahires, All Rights Reserved.
+ */
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world Y!");
 }
